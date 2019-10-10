bee44f5c391cb6b0cb94317ff90f0b8940765e62
mANGANESO <fred.cruz@tectijuana.edu.mx>
2019-10-10 09:26:11 -0700

    Este es mi primer púsh desde mi repo local hacia mi repo en la nube

---
+++ eres_un_campeon/eres_un_campeon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eres_un_campeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola nuevo mundo");
+            Console.ReadKey();
+        }
+
+    }
+}
